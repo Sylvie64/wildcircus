7b68aafddb9cf74d747b1e063466fb6e387d671b
Sylvie64 <sylvie.lecrocq64@gmail.com>
2020-01-31 03:26:39 +0100

    add files after git init

---
+++ wildcircus2/build/precache-manifest.90ec87a0c30eac8fd05e0a317b234ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d854131e909ab1db42c3f9e55d475a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8256feaf60feccd558f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5bab3a01ed77167df878",
+    "url": "/static/js/2.03bfb554.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.03bfb554.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8256feaf60feccd558f",
+    "url": "/static/js/main.83505b51.chunk.js"
+  },
+  {
+    "revision": "b11e167a320e35f11548",
+    "url": "/static/js/runtime-main.014bb781.js"
+  }
+]);
